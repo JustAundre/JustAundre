802733281cf2fef0f3c1f8675e31861972c57f45
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-05-17 22:49:23 -0500

    Minor refactoring
    Added preference persistence.

--- www/scripts.js
+++ www/scripts.js
@@ -1,27 +1,47 @@
 window.addEventListener('DOMContentLoaded', () => {
-	const root = document.querySelector("html")
+	const root = document.documentElement;
 	const configModal = document.querySelector('#config-menu');
 	const toggleTheme = document.querySelector('#toggle-theme');
 	const toggleMotion = document.querySelector('#toggle-motion');
 	const toggleDyslexic = document.querySelector('#toggle-dyslexic');
-	configModal.showModal();
-	configModal.addEventListener('close', () => {
-		if (toggleTheme.checked) {
+	const savedConfig = JSON.parse(localStorage.getItem('userConfig'));
+
+	if (savedConfig) {
+		root.setAttribute('data-theme', savedConfig.theme);
+		root.setAttribute('data-motion', savedConfig.motion);
+		root.setAttribute('data-dyslexic', savedConfig.dyslexic);
+
+		toggleTheme.checked = (savedConfig.theme === 'light');
+		toggleMotion.checked = (savedConfig.motion === 'true');
+		toggleDyslexic.checked = (savedConfig.dyslexic === 'true');
+	} else {
+		if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+			toggleTheme.checked = true;
 			root.setAttribute('data-theme', 'light');
 		} else {
 			root.setAttribute('data-theme', 'dark');
 		}
-
-		if (toggleMotion.checked) {
+		if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+			toggleMotion.checked = true;
 			root.setAttribute('data-motion', 'true');
 		} else {
 			root.setAttribute('data-motion', 'false');
 		}
+		root.setAttribute('data-dyslexic', 'false');
+	}
 
-		if (toggleDyslexic.checked) {
-			root.setAttribute('data-dyslexic', 'true');
-		} else {
-			root.setAttribute('data-dyslexic', 'false');
-		}
+	configModal.showModal();
+	configModal.addEventListener('close', () => {
+		const theme = toggleTheme.checked ? 'light' : 'dark';
+		const motion = toggleMotion.checked ? 'true' : 'false';
+		const dyslexic = toggleDyslexic.checked ? 'true' : 'false';
+
+		root.setAttribute('data-theme', theme);
+		root.setAttribute('data-motion', motion);
+		root.setAttribute('data-dyslexic', dyslexic);
+
+		const config = { theme, motion, dyslexic };
+
+		localStorage.setItem('userConfig', JSON.stringify(config));
 	});
 });
