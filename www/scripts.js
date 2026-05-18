d0db7af9c04b777e7c66a99fd484ee6aeee88957
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-05-17 21:02:18 -0500

    Menu uniformity updates

--- www/scripts.js
+++ www/scripts.js
@@ -1,9 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const root = document.querySelector("html")
 	const configModal = document.querySelector('#config-menu');
-	const toggleTheme = document.getElementById('toggle-theme');
-	const toggleMotion = document.getElementById('toggle-motion');
-	const toggleDyslexic = document.getElementById('toggle-dyslexic');
+	const toggleTheme = document.querySelector('#toggle-theme');
+	const toggleMotion = document.querySelector('#toggle-motion');
+	const toggleDyslexic = document.querySelector('#toggle-dyslexic');
 	configModal.showModal();
 	configModal.addEventListener('close', () => {
 		if (toggleTheme.checked) {
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			root.setAttribute('data-theme', 'dark');
 		}
 
-		if (!toggleMotion.checked) {
+		if (toggleMotion.checked) {
 			root.setAttribute('data-motion', 'true');
 		} else {
 			root.setAttribute('data-motion', 'false');
