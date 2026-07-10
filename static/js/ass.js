534fca7779d18ae8bffd5f734f5381ba8d6fbf50
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-07-10 06:14:08 -0500

    Fixed some accessibility issues for small screens.
    Removed certain bits of home page specific obsolete JS.

--- static/js/ass.js
+++ static/js/ass.js
@@ -1,12 +1,3 @@
-for (const button of document.querySelectorAll('button')) {
-	button.addEventListener('click', () => {
-		button.classList.add('active');
-	});
-	button.addEventListener('animationend', () => {
-		button.classList.remove('active');
-	});
-}
-
 for (const project of document.querySelectorAll('.project-card')) {
 	project.addEventListener('mouseenter', () => {
 		root.setAttribute('style', project.getAttribute('style'))
